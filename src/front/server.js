a60609b87aa692b50d29f38b5dbd4b55cc7dd775
Kirill Nenashew <Kirill.nenashew@gmail.com>
2025-11-02 13:12:19 +0300

    Chats init

--- src/front/server.js
+++ src/front/server.js
@@ -14,19 +14,48 @@ app.get('/', (_, res) => {
     res.sendFile(path.join(__dirname, 'index.html'))
 })
 
+app.get('/chats', (req, res) => {
+    const data = {
+        chats: [
+            {
+                partherId: 'test1',
+                room: '8743ytf9825g4976f/odfb4356tfhygdfgt4653',
+                history: []
+            },
+            {
+                partherId: 'test2',
+                room: '8743ytf9825g4976f/324h0f87245f8',
+                history: []
+            }
+        ]
+    }
+
+    res.send(data)
+})
+
 app.get('/registration', (_, res) => {
     res.sendFile(path.join(__dirname, 'registration.html'))
 })
 
 io.on('connection', (socket) => {
     console.log(`Socket  ${socket.id}`);
+    let describeingRoomObj;
     
     socket.on('disconnect', () => {
         console.log(`Socket отключился ${socket.id}`);
         
     })
+
+    socket.on('enjoy_chat', (describeingRoom) =>{
+        socket.join(describeingRoom.room)
+        describeingRoomObj = describeingRoom
+    })
+
+    socket.on('message', (history) => {
+        socket.to(describeingRoomObj.room).emit('new_history', history)
+    })
 })
 
-server.listen(8000, () => {
+server.listen(9000, () => {
     console.log('Сервер запущен');
 })
